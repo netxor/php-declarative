37d19c0bc29fc9089304d4b1eb8e321fadcf2313
netxor <netxor0b@gmail.com>
2016-01-07 14:58:25 +0600

    Deleted all comments

--- 7. Функция первого класса. Передача как аргументов/product.php
+++ 7. Функция первого класса. Передача как аргументов/product.php
@@ -1,10 +1,3 @@
-# Реализуйте функцию product, которая сворачивает последовательность с помощью переданной функции. Примеры:
-
-# 3 == product(1, 2, function($first, $second) { return $first + $second; }) // sum
-# -4 == product(1, 3, function($first, $second) { return $first - $second; }) // minus
-
-# То есть вместо жестко заданной операции, например, сложения, нужно рекурсивно применять переданную функцию.
-
 <?php
 
 function product($num1, $num2, $func) {
