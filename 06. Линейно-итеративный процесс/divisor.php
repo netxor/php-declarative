9615aea02331c6816be648f75e22f97a6a08da66
netxor <netxor0b@gmail.com>
2016-03-07 23:08:15 +0600

    add solution for smallest divisor

--- 06. Линейно-итеративный процесс/divisor.php
+++ 06. Линейно-итеративный процесс/divisor.php
@@ -1,10 +1,19 @@
 <?php
 
-function smallestDivisor($num) {
+namespace App\Divisor;
 
-
-
+function smallestDivisor($num)
+{
+    // BEGIN (write your solution here)
+    $sdIter = function($num, $acc) use (&$sdIter) {
+        if ($num == $acc) {
+            return 1;
+        }
+        if ($num % $acc == 0) {
+            return $acc;
+        }
+        return $sdIter($num, $acc + 1);
+    };
+    return $sdIter($num, 2);
+    // END
 }
-
-
- ?>
